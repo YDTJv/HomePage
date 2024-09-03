1c0df5206907849548f00c747a7c0fdddddb8dcc
r0 <taddeowang123@gmail.com>
2024-09-03 14:40:00 +0800

    update

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,23 @@
+<!--
+ * @Description: 
+ * @Author: DSTBP
+ * @Date: 2024-09-03 12:07:14
+ * @LastEditTime: 2024-09-03 14:39:19
+ * @LastEditors: DSTBP
+-->
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+    ：<br><br>
+
+    朋友你好！既然点进这里，想必你想要了解了解我吧！<br>
+    简单介绍一下：本人大学牲，没啥才艺，念首诗吧<br><br>
+    
+    钗头凤·红酥手<br>
+    红酥手，黄藤酒。满城春色宫墙柳；东风恶，欢情薄，一怀愁绪，几年离索，错，错，错！<br>
+    春如旧，人空瘦。泪痕红浥鲛绡透；桃花落，闲池阁，山盟虽在，锦书难托，莫，莫，莫！<br><br>
+    
+    如果想要更多的了解我一些，就请进入我的个人博客，找到我的个人介绍页吧~<br>
+  </div>
 </template>
 
 <style lang="scss" scoped>
