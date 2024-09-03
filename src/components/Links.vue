00f2a20a55bf81a72b31159e4c1b4902d4774fb9
DSTBP <93864880+DSTBP@users.noreply.github.com>
2024-09-03 11:19:23 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -46,7 +46,6 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Music } from "@vicons/fa"; // 注意使用正确的类别
-import { BarChart } from "@vicons/ionicons5";
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -74,7 +73,7 @@ const siteIcon = {
   Fire,
   LaptopCode,
   Music,
-  BarChart
+  ChartBar
 };
 
 // 链接跳转
